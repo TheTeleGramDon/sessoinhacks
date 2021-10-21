a2ca077b41bb561f9eebe3b7b58db58c76d81f9c
SAD_KING <89540820+Mohan745@users.noreply.github.com>
2021-10-21 16:21:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("git clone https://github.com/Mohan745/sessoinhacks")
+os.system("git clone github.com/Mohan745/sessoinhacks")
 
 os.system("cd sessoinhacks && python bot.py")
 
